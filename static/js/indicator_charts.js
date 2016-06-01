dad31d80dfadc40c04ca1ad6dc28b5192bcfac59
egzontina <egzontina.krasniqi@gmail.com>
2016-06-01 16:58:45 +0200

    minor fix

--- static/js/indicator_charts.js
+++ static/js/indicator_charts.js
@@ -255,7 +255,7 @@ $(document).ready(function(){
     } else {
     $("#chart-div-1").append("<div id='chart1' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
 		$("#chart-div-2").append("<div id='chart2' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
-		$("chart-div-3").append("<div id='chart33' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
+		$("chart-div-3").append("<div id='chart3' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
 		$("chart-div-4").append("<div id='chart4' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
 		$("chart-div-5").append("<div id='chart5' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
 		$("chart-div-6").append("<div id='chart6' style='margin: auto; min-width: 310px; max-width: 800px; width: 100%; height: 400px;'></div>");
@@ -274,10 +274,10 @@ $(document).ready(function(){
 	// Draw Charts
 	drawBarChart('chart1', '1', true);
 	drawBarChart('chart2', '2', false);
-	drawBarChart('chart33', '3', false);
-	drawBarChart('chart4', '4', true);
-	drawBarChart('chart5', '5', true);
-	drawBarChart('chart6', '6', false);
+	drawBarChart('chart3', '1', false);
+	drawBarChart('chart4', '2', true);
+	drawBarChart('chart5', '1', true);
+	drawBarChart('chart6', '2', false);
 	drawBarChart('chart6-1', '6_1', true);
 	drawBarChart('chart6-2', '6_2', false);
 	drawBarChart('chart7', '7', false);
